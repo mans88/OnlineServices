352b71a5e25fcd98768a35ff5db384e49d2c3265
TheRealDelep <41925548+TheRealDelep@users.noreply.github.com>
2020-01-22 16:14:11 +0100

    not implementing sessionRepository

--- Application Layer/UserServices/UserServices.DataLayer/Repositories/SessionRepository.cs
+++ Application Layer/UserServices/UserServices.DataLayer/Repositories/SessionRepository.cs
@@ -6,59 +6,58 @@ using System.Text;
 
 namespace RegistrationServices.DataLayer.Repositories
 {
-    //class SessionRepository : IRSSessionRepository
-    //{
-    //    private readonly RegistrationServicesContext sessionContext;
+    class SessionRepository : IRSSessionRepository
+    {
+        private readonly RegistrationServicesContext sessionContext;
 
-    //    public SessionRepository(RegistrationServicesContext Context)
-    //    {
-    //        sessionContext = Context ?? throw new ArgumentNullException($"{nameof(Context)} in UserRepository");
-    //    }
+        public SessionRepository(RegistrationServicesContext Context)
+        {
+            sessionContext = Context ?? throw new ArgumentNullException($"{nameof(Context)} in UserRepository");
+        }
 
-    //    public SessionTO Add(SessionTO Entity)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
+        public SessionTO Add(SessionTO Entity)
+        {
+            throw new NotImplementedException();
+        }
 
-    //    public IEnumerable<SessionTO> GetAll()
-    //    {
-    //        throw new NotImplementedException();
-    //    }
+        public IEnumerable<SessionTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
 
-    //    public SessionTO GetById(int Id)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
+        public SessionTO GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
 
-    //    public IEnumerable<DateTime> GetDates(SessionTO session)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
+        public IEnumerable<DateTime> GetDates(SessionTO session)
+        {
+            throw new NotImplementedException();
+        }
 
-    //    public IEnumerable<UserTO> GetStudents(SessionTO session)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
+        public IEnumerable<UserTO> GetStudents(SessionTO session)
+        {
+            throw new NotImplementedException();
+        }
 
-    //    public bool Remove(SessionTO entity)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
+        public bool Remove(SessionTO entity)
+        {
+            throw new NotImplementedException();
+        }
 
-    //    public bool Remove(int Id)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
+        public bool Remove(int Id)
+        {
+            throw new NotImplementedException();
+        }
 
-    //    public SessionTO Update(SessionTO Entity)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
+        public SessionTO Update(SessionTO Entity)
+        {
+            throw new NotImplementedException();
+        }
 
-    //    public IEnumerable<SessionTO> GetByStudent(UserTO student)
-    //    {
-    //        => sessionContext.UserSessions
-    //            .
-    //    }
-    //}
+        public IEnumerable<SessionTO> GetByStudent(UserTO student)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
