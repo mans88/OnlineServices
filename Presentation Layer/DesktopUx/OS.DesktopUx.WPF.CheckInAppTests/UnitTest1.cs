30a6567337d09ed80054943074903414fbd8616b
Christian P. Gyssels <gyssels@dekeract.com>
2020-02-09 21:12:51 +0100

    Demo WPF app for Attendee Check-in (#85)
    Starting the WPF example...

---
+++ Presentation Layer/DesktopUx/OS.DesktopUx.WPF.CheckInAppTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OnlineServices.DesktopUx.WpfCore.CheckInAppTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
